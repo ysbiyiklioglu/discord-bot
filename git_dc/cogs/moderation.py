99d75686e697f0b5dd5a9e8ce8f7becc628527e9
yasin bıyıklıoğlu <ysbiyiklioglu@gmail.com>
2024-08-02 14:34:53 +0300

    Update moderation.py

--- git_dc/cogs/moderation.py
+++ git_dc/cogs/moderation.py
@@ -4,10 +4,13 @@ from discord.ext import commands
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
     @commands.command()
-    async def selamver(self, ctx):
-        await ctx.send("merhaba")  # Kullanıcıya "merhaba" mesajı gönderir
+    async def bilgi(self, ctx):
+     embed=discord.Embed(title="Bot Hakkında", color=0xc22e2e)
+     embed.add_field(name="bot_version", value="0.1", inline=True)
+     embed.add_field(name="bot_name", value="superbot", inline=True)
+     embed.add_field(name="bot_creator", value="windness", inline=True)
+     await ctx.send(embed=embed)    
 
 async def setup(bot):
     await bot.add_cog(Moderation(bot))
